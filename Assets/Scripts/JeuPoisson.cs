af391deeff3c4c71ef3889b998c89e58973fa872
Obadia11 <izied99ek3n@gmail.com>
2025-02-07 14:53:29 -0500

    Vector3

--- Assets/Scripts/JeuPoisson.cs
+++ Assets/Scripts/JeuPoisson.cs
@@ -13,12 +13,15 @@ public class JeuPoisson : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+       
         //Debug.Log(poidsSaumon);
     }
 
     // Update is called once per frame
     void Update()
     {
+         Vector3 deplacement = new Vector3(1f, 0f, 0f);
+        _sardine.transform.position += deplacement * 4f * Time.deltaTime;
         //Debug.Log(poidsSaumon);
 
     }
